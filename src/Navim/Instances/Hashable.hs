faa59a58eb5bbe4ae6d7901683ccb5ad9893255a
Gary Feng <gary.feng@uwaterloo.ca>
2020-04-27 00:33:15 -0400

    Derive Hashable instances for Key and Modifier from Generic

--- src/Navim/Instances/Hashable.hs
+++ src/Navim/Instances/Hashable.hs
@@ -1,15 +1,11 @@
+{-# LANGUAGE DeriveGeneric #-}
+
 module Navim.Instances.Hashable where
 
 import Data.Hashable
 
 import Graphics.Vty.Input.Events
 
-instance Hashable Key where
-    hashWithSalt n (KChar c) = n + fromEnum c
-    hashWithSalt n _ = n + 10
+instance Hashable Key
 
-instance Hashable Modifier where
-    hashWithSalt n MShift = n
-    hashWithSalt n MCtrl  = n + 1
-    hashWithSalt n MMeta  = n + 2
-    hashWithSalt n MAlt   = n + 3
+instance Hashable Modifier
